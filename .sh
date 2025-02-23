4a50a13108e40d86c6970ef2e36501b42d7736ef
ProjectGleb <gleb.studios@gmail.com>
2025-02-23 14:06:48 -0600

    Add lfs support

---
+++ .sh
@@ -0,0 +1,13 @@
+# For Mac (using Homebrew)
+brew install git-lfs
+
+# For Ubuntu/Debian
+sudo apt install git-lfs
+
+# For Windows (using Chocolatey)
+choco install git-lfs
+
+
+#TO INSTALL THE DATASET RUN:
+git lfs install
+git lfs pull
